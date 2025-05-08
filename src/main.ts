856860b94fa025393483a9ce45536cca68ddfc6a
pikiey04 <pikiey04@gmail.com>
2025-05-08 22:56:31 +0800

    change on main and package

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   const host = `0.0.0.0`;
 
   await app.listen(port, host);
